b2fb6d2adb44b517ffd2cf6e08f3b5eefb7debb2
Thomas Weidmaier <tommyweid911@gmail.com>
2018-10-07 21:46:09 -0700

    init commit
    initial unzipped version

---
+++ Assignment1/Code/EvaluationsStub.py
@@ -0,0 +1,54 @@
+
+
+def __CheckEvaluationInput(y, yPredicted):
+    # Check sizes
+    if(len(y) != len(yPredicted)):
+        raise UserWarning("Attempting to evaluate between the true labels and predictions.\n   Arrays contained different numbers of samples. Check your work and try again.")
+
+    # Check values
+    valueError = False
+    for value in y:
+        if value not in [0, 1]:
+            valueError = True
+    for value in yPredicted:
+        if value not in [0, 1]:
+            valueError = True
+
+    if valueError:
+        raise UserWarning("Attempting to evaluate between the true labels and predictions.\n   Arrays contained unexpected value. Must be 0 or 1.")
+
+def Accuracy(y, yPredicted):
+    __CheckEvaluationInput(y, yPredicted)
+
+    correct = []
+    for i in range(len(y)):
+        if(y[i] == yPredicted[i]):
+            correct.append(1)
+        else:
+            correct.append(0)
+
+    return sum(correct)/len(correct)
+
+def Precision(y, yPredicted):
+    pass
+
+def Recall(y, yPredicted):
+    pass
+
+def FalseNegativeRate(y, yPredicted):
+    pass
+
+def FalsePositiveRate(y, yPredicted):
+    pass
+
+def ConfusionMatrix(y, yPredicted):
+    pass
+
+def ExecuteAll(y, yPredicted):
+    print(ConfusionMatrix(y, yPredicted))
+    print("Accuracy:", Accuracy(y, yPredicted))
+    print("Precision:", Precision(y, yPredicted))
+    print("Recall:", Recall(y, yPredicted))
+    print("FPR:", FalsePositiveRate(y, yPredicted))
+    print("FNR:", FalseNegativeRate(y, yPredicted))
+    
---
+++ Assignment1/Code/StartingPoint1.py
@@ -0,0 +1,51 @@
+
+import Assignment1Support
+import EvaluationsStub
+
+### UPDATE this path for your environment
+kDataPath = "..\\..\\..\\Datasets\\SMS Spam\\extracted\\SMSSpamCollection"
+
+(xRaw, yRaw) = Assignment1Support.LoadRawData(kDataPath)
+
+(xTrainRaw, yTrainRaw, xTestRaw, yTestRaw) = Assignment1Support.TrainTestSplit(xRaw, yRaw)
+
+print("Train is %f percent spam." % (sum(yTrainRaw)/len(yTrainRaw)))
+print("Test is %f percent spam." % (sum(yTestRaw)/len(yTestRaw)))
+
+(xTrain, xTest) = Assignment1Support.Featurize(xTrainRaw, xTestRaw)
+yTrain = yTrainRaw
+yTest = yTestRaw
+
+############################
+import MostCommonModel
+
+model = MostCommonModel.MostCommonModel()
+model.fit(xTrain, yTrain)
+yTestPredicted = model.predict(xTest)
+
+print("### 'Most Common' model")
+
+EvaluationsStub.ExecuteAll(yTest, yTestPredicted)
+
+############################
+import SpamHeuristicModel
+model = SpamHeuristicModel.SpamHeuristicModel()
+model.fit(xTrain, yTrain)
+yTestPredicted = model.predict(xTest)
+
+print("### Heuristic model")
+
+EvaluationsStub.ExecuteAll(yTest, yTestPredicted)
+
+############################
+# import LogisticRegressionModel
+# model = LogisticRegressionModel.LogisticRegressionModel()
+
+# print("Logistic regression model")
+# for i in [50000]:
+#     model.fit(xTrain, yTrain, iterations=i, step=0.01)
+#     yTestPredicted = model.predict(xTest)
+    
+#     print("%d, %f, %f, %f" % (i, model.weights[1], model.loss(xTest, yTest), EvaluationStub.Accuracy(yTest, yTestPredicted)))
+
+# EvaluationsStub.ExecuteAll(yTest, yTestPredicted)
