d07d9dea8016b028f20251842cb2bc2be11a95bf
Thomas Weidmaier <tommyweid911@gmail.com>
2018-10-19 07:54:33 -0700

    Cross  Validation
    implement cross validation HW

---
+++ Assignment1/Code/BagOfWordsModel.py
@@ -0,0 +1,121 @@
+import math
+
+class BagOfWordsModel(object):
+    """A model that calculates the logsitics regression analysis."""
+
+    def __init__(self):
+        self.vocabulary = []
+        pass
+
+    def fillVocabulary(self, x):
+        cnt = 0
+        for example in x:
+            curr_words = example.split()
+            for word in curr_words:
+                if word not in self.vocabulary:
+                    cnt += 1
+                    #print(word)
+                    self.vocabulary.append(word)
+
+    def FrequencyFeatureSelection(self, x, n):
+
+        vocabCount = {}
+        for vocab in self.vocabulary:
+            vocabCount[vocab] = 0
+
+        #features = []
+        for example in x:
+            curr_words = example.split()
+            #curr_features = []
+            for word in curr_words:
+                if word in vocabCount:
+                    vocabCount[word] += 1
+                    #print(word,":", vocabCount[word])
+                    #curr_features.append(1)
+                #else:
+                #    curr_features.append(0)
+            #features.append(curr_features)
+        #print(features)
+
+        sorted_vocabCount = sorted(vocabCount.items(), key=lambda kv: kv[1], reverse=True)
+        topn_results = sorted_vocabCount[:n]
+        return topn_results
+
+    def MutualInformationFeatureSelection(self, x, y, n):
+        cnt = len(y)
+        pos_cnt = sum(y)
+        neg_cnt = cnt - pos_cnt
+        mutualInfo = {}
+
+        for word in self.vocabulary:
+            word_present = word_pos = word_neg = noword_pos = noword_neg = currMI = 0
+            
+            for i in range(cnt):
+                example = x[i].split()
+
+                if word in example:
+                    word_present += 1
+                    if y[i] == 1:
+                        word_pos += 1
+                    else:
+                        word_neg += 1
+                else:
+                    if y[i] == 1:
+                        noword_pos += 1
+                    else:
+                        noword_neg += 1
+
+            word_missing = cnt - word_present
+
+            #Calculate P(word,1) and P(word,0)
+            prob_word_pos =  self.prob_func(word_pos, cnt)
+            prob_word_neg =  self.prob_func(word_neg, cnt)
+            prob_noword_pos =  self.prob_func(noword_pos, cnt)
+            prob_noword_neg =  self.prob_func(noword_neg, cnt)
+            prob_pos = self.prob_func(pos_cnt, cnt)
+            prob_neg = self.prob_func(neg_cnt, cnt)
+            prob_word = self.prob_func(word_present, cnt)
+            prob_noword = self.prob_func(word_missing, cnt)
+
+            currMI += prob_word_pos * math.log2(prob_word_pos / (prob_word * prob_pos) )
+            currMI += prob_word_neg * math.log2(prob_word_neg / (prob_word * prob_neg) )
+            currMI += prob_noword_pos * math.log2(prob_noword_pos / (prob_noword * prob_pos) )
+            currMI += prob_noword_neg * math.log2(prob_noword_neg / (prob_noword * prob_neg) )
+            mutualInfo[word] = currMI
+
+        sorted_mutualInfo = sorted(mutualInfo.items(), key=lambda kv: kv[1], reverse=True)
+        topn_results = sorted_mutualInfo[:n]
+        return topn_results
+
+    def prob_func(self, observed, total):
+        return (observed + 1.0) / (total + 2.0)
+
+    def FeaturizeByWords(self, xTrainRaw, xTestRaw, words):
+        # featurize the training data, may want to do multiple passes to count things.
+        xTrain = []
+        for x in xTrainRaw:
+            features = []
+            # Have features for a few words
+            for key_word in words:
+                if key_word in x:
+                    features.append(1)
+                else:
+                    features.append(0)
+            #print(features)
+            xTrain.append(features)
+
+        # now featurize test using any features discovered on the training set. Don't use the test set to influence which features to use.
+        xTest = []
+        for x in xTestRaw:
+            features = []
+
+            # Have features for a few words
+            for word in words:
+                if word in x:
+                    features.append(1)
+                else:
+                    features.append(0)
+            #print(features)
+            xTest.append(features)
+
+        return (xTrain, xTest)
---
+++ Assignment2/Code/StartingPoint2.py
@@ -0,0 +1,145 @@
+
+import Assignment1Support
+import EvaluationsStub
+import numpy as np
+import time
+
+### UPDATE this path for your environment
+kDataPath = "..\\Data\\SMSSpamCollection"
+
+(xRaw, yRaw) = Assignment1Support.LoadRawData(kDataPath)
+
+(xTrainRaw, yTrainRaw, xTestRaw, yTestRaw) = Assignment1Support.TrainTestSplit(xRaw, yRaw)
+
+print("Train is %f percent spam." % (sum(yTrainRaw)/len(yTrainRaw)))
+print("Test is %f percent spam." % (sum(yTestRaw)/len(yTestRaw)))
+
+(xTrain, xTest) = Assignment1Support.Featurize(xTrainRaw, xTestRaw)
+yTrain = yTrainRaw
+yTest = yTestRaw
+
+############################
+import MostCommonModel
+
+model = MostCommonModel.MostCommonModel()
+model.fit(xTrain, yTrain)
+yTestPredicted = model.predict(xTest)
+
+print("### 'Most Common' model")
+
+EvaluationsStub.ExecuteAll(yTest, yTestPredicted)
+
+############################
+import SpamHeuristicModel
+model = SpamHeuristicModel.SpamHeuristicModel()
+model.fit(xTrain, yTrain)
+yTestPredicted = model.predict(xTest)
+
+print("### Heuristic model")
+
+EvaluationsStub.ExecuteAll(yTest, yTestPredicted)
+
+############################
+import LogisticRegressionModel
+model = LogisticRegressionModel.LogisticRegressionModel()
+
+print("Logistic regression model")
+
+#np_xTrain = np.insert(np.asarray(xTrain), 0, 1, axis = 1)
+#np_xTest = np.insert(np.asarray(xTest), 0, 1, axis = 1)
+np_yTrain = np.asarray(yTrain)
+np_yTest = np.asarray(yTest)
+
+#cntList = [50000]
+#for i in cntList:
+#    start = time.time()
+#    model.fit(np_xTrain, np_yTrain, iterations=i, step=0.01)
+#    yTestPredicted = model.predict(np_xTest)
+#    end = time.time()
+#    print("%d, %f, %f, %f" % (i, model.weights[1], model.loss(np_xTest, np_yTest), EvaluationsStub.Accuracy(np_yTest, yTestPredicted)))
+#    EvaluationsStub.ExecuteAll(np_yTest, yTestPredicted)
+#    print("Model runtime:", end-start)
+
+#############################
+import BagOfWordsModel
+bagmodel = BagOfWordsModel.BagOfWordsModel()
+"""
+print("Running Bag of Words")
+topN = 10
+
+bagmodel.fillVocabulary(xTrainRaw)
+results = bagmodel.FrequencyFeatureSelection(xTrainRaw, topN)
+#results = bagmodel.MutualInformationFeatureSelection(xTrainRaw, yTrainRaw, topN)
+
+print(results)
+
+words = []
+for i in range(topN):
+    words.append(results[i][0])
+
+print("Logistic regression model - Using Bag of model")
+xTrainMI, xTestMI = bagmodel.FeaturizeByWords(xTrainRaw, xTestRaw, words)
+
+#np_xTrainFullFeautures = np.hstack((np.asarray(xTrain), np.asarray(xTrainMI)))
+#np_xTestFullFeautures = np.hstack((np.asarray(xTest), np.asarray(xTestMI)))
+np_xTrainFullFeautures = np.asarray(xTrainMI)
+np_xTestFullFeautures = np.asarray(xTestMI)
+
+np_xTrain = np.insert(np_xTrainFullFeautures, 0, 1, axis = 1)
+np_xTest = np.insert(np_xTestFullFeautures, 0, 1, axis = 1)
+
+for i in [50000]:
+    model.fit(np_xTrain, np_yTrain, iterations=i, step=0.01)
+    yTestPredicted = model.predict(np_xTest)
+
+    print("%d, %f, %f, %f" % (i, model.weights[1], model.loss(np_xTest, np_yTest), EvaluationsStub.Accuracy(np_yTest, yTestPredicted)))
+    EvaluationsStub.ExecuteAll(np_yTest, yTestPredicted)
+
+    accuracy = EvaluationsStub.Accuracy(np_yTest, yTestPredicted)
+    y_len =  len(yTestPredicted)
+    (lowerBounds, upperBounds) = EvaluationsStub.calculate95PercentConfidenceBounds(accuracy, y_len)
+    print(i,": ", accuracy, " l:", lowerBounds, " u:", upperBounds)
+"""
+#############################
+import CrossValidationSupport
+
+import BagOfWordsModel
+bagmodel = BagOfWordsModel.BagOfWordsModel()
+
+topN = 10
+k = 5
+full_cnt = len(xTrainRaw)
+correct_cnt = 0
+for i in range(k):
+    (xTrainOnRaw, yTrainOnRaw, xValidateOnRaw, yValidateOnRaw) = CrossValidationSupport.DefineDataBounds(xTrainRaw, yTrainRaw, k, i)
+
+    bagmodel.fillVocabulary(xTrainOnRaw)
+    results = bagmodel.FrequencyFeatureSelection(xTrainOnRaw, topN)
+    #results = bagmodel.MutualInformationFeatureSelection(xTrainOnRaw, yTrainOnRaw, topN)
+
+    words = []
+    for j in range(topN):
+        words.append(results[j][0])
+    print(words)
+    
+    xTrainMI, xValidateMI = bagmodel.FeaturizeByWords(xTrainOnRaw, xValidateOnRaw, words)
+
+    np_xTrain = np.insert(np.asarray(xTrainMI), 0, 1, axis = 1)
+    np_xValidate = np.insert(np.asarray(xValidateMI), 0, 1, axis = 1)
+
+    np_yTrain = np.asarray(yTrainOnRaw)
+    np_yValidate = np.asarray(yValidateOnRaw)
+
+    for j in [50000]:
+        model.fit(np_xTrain, np_yTrain, iterations=j, step=0.01)
+        yValidatePredicted = model.predict(np_xValidate)
+
+    validateLen = len(yValidatePredicted)
+    for j in range(validateLen):
+        if yValidatePredicted[j] == np_yValidate[j]:
+            correct_cnt += 1
+    print("Correct after ",i," is ", correct_cnt)
+
+accuracy = correct_cnt / full_cnt
+(lowerBounds, upperBounds) = EvaluationsStub.calculate95PercentConfidenceBounds(accuracy, full_cnt)
+print("MI: ", accuracy, " l:", lowerBounds, " u:", upperBounds) 
